4fe1ee5f3b156b66fa9e62d3cbc8d92c4c6b1950
Shade-v2006 <Shade-v2006@yandex.ru>
2024-02-14 22:31:43 +0700

    To do string in Array

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+    Задание 2. 
+    На основе символов строки (тип string) сформировать массив
+    символов (тип char[]). Вывести массив на экран.
+    Указание: Метод строки ToCharArray() не использовать.
+*/
+
+string str = "Hello!!!";
+char[] MakeCharsArray(string str)
+{
+    int size = str.Length;
+    char[] word = new char[size];
+    for (int i = 0; i < str.Length; i++)
+    {
+        word[i] = str[i];
+    }
+    return word;
+}
+char[] word = MakeCharsArray(str);
+foreach (char e in word)
+{
+    System.Console.Write($"{e} ");
+}
