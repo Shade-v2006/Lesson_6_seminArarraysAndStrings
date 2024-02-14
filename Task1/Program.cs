2d4246a49a839f7f50bf4556263c1fd6ab801051
Shade-v2006 <Shade-v2006@yandex.ru>
2024-02-14 22:17:22 +0700

    To do string from Array

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+    Задайте массив символов (тип char []). Создайте строку из
+    символов этого массива.
+    Указание:
+    Конструктор строки вида string(char []) не использовать.
+    Пример:
+    [‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+*/
+char[] chars = { 'a', 'b', 'c', 'd' };
+
+string PrString(char[] chars)
+{
+    string sim = "";
+    foreach (char e in chars)
+    {
+        sim = sim + e;
+    }
+    return sim;
+
+}
+string sim = PrString(chars);
+System.Console.WriteLine(value: sim);
